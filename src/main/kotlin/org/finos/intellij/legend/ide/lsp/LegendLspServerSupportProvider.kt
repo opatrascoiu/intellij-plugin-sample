380166332a1ffc5f041570d5a6cbfcfc14466b4c
Octavian Patrascoiu <opatrascoiu@yahoo.com>
2024-03-01 09:22:34 +0000

    Fix the command line

--- src/main/kotlin/org/finos/intellij/legend/ide/lsp/LegendLspServerSupportProvider.kt
+++ src/main/kotlin/org/finos/intellij/legend/ide/lsp/LegendLspServerSupportProvider.kt
@@ -1,11 +1,14 @@
 package org.finos.intellij.legend.ide.lsp
 
+import com.intellij.execution.ExecutionException
 import com.intellij.execution.configurations.GeneralCommandLine
+import com.intellij.openapi.application.PluginPathManager
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.platform.lsp.api.LspServerSupportProvider
 import com.intellij.platform.lsp.api.ProjectWideLspServerDescriptor
 import org.finos.intellij.legend.ide.completion.LegendLspCompletionSupport
+import java.io.File
 
 internal class LegendLspServerSupportProvider : LspServerSupportProvider {
     override fun fileOpened(project: Project, file: VirtualFile, serverStarter: LspServerSupportProvider.LspServerStarter) {
@@ -17,19 +20,29 @@ internal class LegendLspServerSupportProvider : LspServerSupportProvider {
 
 private const val EXEC = "java"
 
-private const val ARGS = "-jar ./server/legend-engine-ide-lsp-server-shaded-0.0.9-SNAPSHOT.jar ./server/pom.xml"
+private const val LEGEND_SERVER_PATH = "server";
+
+private const val LEGEND_SERVER_JAR = "legend-engine-ide-lsp-server-shaded-0.0.9-SNAPSHOT.jar"
 
 private class LegendLspServerDescriptor(project: Project) : ProjectWideLspServerDescriptor(project, "Pure") {
 
     override fun isSupportedFile(file: VirtualFile) = file.extension == "pure"
-//    override fun createCommandLine() = GeneralCommandLine(EXEC, ARGS)
 
     override fun createCommandLine(): GeneralCommandLine {
+        val lsp = getPluginDirectory(javaClass, LEGEND_SERVER_PATH)
+        LOG.info("Path server path = '{}' exists = '{}'".format(lsp?.path, lsp?.exists()))
+        if (lsp == null || !lsp.exists()) {
+            // broken plugin installation?
+            throw ExecutionException("Cannot find path for the Legend server" + lsp?.path)
+        }
+
         return GeneralCommandLine().apply {
             withParentEnvironmentType(GeneralCommandLine.ParentEnvironmentType.CONSOLE)
+            withWorkDirectory(lsp.path)
             withCharset(Charsets.UTF_8)
             withExePath(EXEC)
-            addParameters(ARGS)
+            // TODO configure the pom
+            withParameters("-jar", LEGEND_SERVER_JAR)
         }
     }
 
@@ -45,18 +58,22 @@ private class LegendLspServerDescriptor(project: Project) : ProjectWideLspServer
 //    override val lspHoverSupport = false
 
     // since 2023.2
-    override val lspGoToDefinitionSupport = false;
+//    override val lspGoToDefinitionSupport = false;
 
     override val lspCompletionSupport = LegendLspCompletionSupport();
 
-    override val lspDiagnosticsSupport = null;
+//    override val lspDiagnosticsSupport = null;
 
-    override val lspCodeActionsSupport = null;
+//    override val lspCodeActionsSupport = null;
 
-    override val lspCommandsSupport = null;
+//    override val lspCommandsSupport = null;
 
     // since 2023.3:
 //    override val lspFormattingSupport = null;
 
 //    override val lspHoverSupport = true;
+
+    fun getPluginDirectory(pluginClass: Class<*>, resourceName: String): File? {
+        return PluginPathManager.getPluginResource(pluginClass, resourceName)
+    }
 }
